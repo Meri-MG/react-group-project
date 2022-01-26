e1316fac5cc782b68cb5d5574a90869c4626847f
Meri <mgogi13@freeuni.edu.ge>
2022-01-26 17:33:44 +0400

    Add myProfile component and show reserved rockets

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Navigation from './components/Navbar';
 import Rockets from './components/Rockets';
 import Dragon from './components/Dragons/Dragon';
 import MissionList from './components/MissionList';
+import MyProfile from './components/MyProfile';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,6 +19,7 @@ ReactDOM.render(
           <Route path="/" element={<Rockets />} />
           <Route path="/missions" element={<MissionList />} />
           <Route path="/Dragons" element={<Dragon />} />
+          <Route path="/myProfile" element={<MyProfile />} />
         </Routes>
       </BrowserRouter>
     </Provider>
