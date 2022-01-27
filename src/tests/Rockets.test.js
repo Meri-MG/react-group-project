1aec5a7c69be96f558d9cac72f078d86feb08c7e
Meri <mgogi13@freeuni.edu.ge>
2022-01-27 16:35:39 +0400

    Update Readme file

--- src/tests/Rockets.test.js
+++ src/tests/Rockets.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
+import * as ReactRedux from 'react-redux';
 import store from '../redux/configureStore';
 import Rockets from '../components/Rockets/Rockets';
+
+// useDispatch returns a function which we are mocking here
+const mockDispatch = jest.fn();
+
+beforeAll(() => {
+  ReactRedux.useDispatch = jest.fn().mockImplementation(() => mockDispatch);
+});
+
+beforeEach(() => {
+  ReactRedux.useDispatch.mockClear();
+});
 
 describe('Rockets Component test', () => {
   it('Renders Rocket Page', () => {
