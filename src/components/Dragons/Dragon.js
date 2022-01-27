170ce4f057bec2bd8287f5ae1072ae7246c74ec1
Meri <mgogi13@freeuni.edu.ge>
2022-01-27 20:18:47 +0400

    Edit styles to buttons

--- src/components/Dragons/Dragon.js
+++ src/components/Dragons/Dragon.js
@@ -1,20 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import store from '../../redux/configureStore';
 import Reserve from './Reserve';
 import classes from './Dragons.module.css';
+import styles from './Reserve.module.css';
 import { reserveDragon } from '../../redux/dragons/dragons';
 
 const getDataFromApi = 'getDataFromApi';
 function Dragon() {
+  const [reserveStyle, setReserveStyle] = useState(true);
   const dragons = useSelector((state) => state.dragonReducer);
-  useEffect(
-    () => {
-      store.dispatch({ type: getDataFromApi });
-    }, [],
-  );
+  useEffect(() => {
+    store.dispatch({ type: getDataFromApi });
+  }, []);
   const reserveHandler = (el) => {
+    setReserveStyle(false);
     store.dispatch(reserveDragon(el));
   };
   return (
@@ -25,16 +26,30 @@ function Dragon() {
             <span className={classes.alignText}>
               <img key={uuidv4()} src={el.flickr_images[0]} alt={el.name} />
               <span>
-                <h2>
-                  {el.name}
-                </h2>
+                <h2>{el.name}</h2>
+
                 <p>
+                  {el.reserved ? (
+                    <span className={classes.badge}>Reserved</span>
+                  ) : (
+                    ''
+                  )}
                   {el.description}
                 </p>
+
+                <div>
+                  <Reserve
+                    styleing={
+                      reserveStyle ? styles.button : styles.buttonCancel
+                    }
+                    reserveHandler={() => reserveHandler(el)}
+                    content={
+                      el.reserved ? 'Cancel reservation' : 'Reserve dragon'
+                    }
+                  />
+                </div>
               </span>
             </span>
-            <span><Reserve reserveHandler={() => reserveHandler(el)} content={el.reserved ? 'Cancel reservation' : 'Reserve dragon'} /></span>
-            {el.reserved ? <span className={classes.badge}>Reserved</span> : ''}
           </li>
         ))}
       </ul>
